25d9220ad1317b7e057c466fa229ee4646bad59e
Erik Ford <ekford18@gmail.com>
2020-03-27 16:19:21 -0700

    finished ID and java, works

--- app.js
+++ app.js
@@ -22,6 +22,27 @@ const nounTwo = document.getElementById('noun-two');
 const nounPlural = document.getElementById('noun-plural');
 // well need spans as well to put the words into the madlib
 const adjectiveInputOneSpan = document.getElementById('adjective-one-span');
+const nounCreatureSpan = document.getElementById('noun-creature-span');
+const nounCreatureSpanTwo = document.getElementById('noun-creature-span-2');
+const nameOneSpan = document.getElementById('name-one-span');
+const placeOneSpan = document.getElementById('place-one-span');
+const placeOneSpanTwo = document.getElementById('place-one-span-2');
+const yourNameSpan = document.getElementById('your-name-span');
+const nounItemOneSpan = document.getElementById('noun-item-one-span');
+const nounItemOneSpanTwo = document.getElementById('noun-item-one-2');
+const nounItemTwoSpan = document.getElementById('noun-item-two-span');
+const nounItemTwoSpanTwo = document.getElementById('noun-item-two-span-2');
+const adjectiveInputTwoSpan = document.getElementById('adjective-input-two-span');
+const adjectiveInputThreeSpan = document.getElementById('adjective-input-three-span');
+const animalOneSpan = document.getElementById('animal-one-span');
+const verbOneSpan = document.getElementById('verb-one-span');
+const adjectiveInputFourSpan = document.getElementById('adjective-input-four-span');
+const adjectiveInputFiveSpan = document.getElementById('adjective-input-five-span');
+const nameTwoSpan = document.getElementById('name-two-span');
+const nounOneSpan = document.getElementById('noun-one-span');
+const nounTwoSpan = document.getElementById('noun-two-span');
+const nounPluralSpan = document.getElementById('noun-plural-span');
+
 //well need the container that holds the madlib
 const madlibContainer = document.getElementById('madlib-container');
 // well need the container that holds all the inputs from the USER
@@ -53,6 +74,28 @@ function readersAnswers() {
     const nounPluralValue = nounPlural.value;
 
     adjectiveInputOneSpan.textContent = adjectiveInputOneValue;
+    nounCreatureSpan.textContent = nounCreatureOneValue;
+    nounCreatureSpanTwo.textContent = nounCreatureOneValue;
+    nameOneSpan.textContent = nameOneValue;
+    placeOneSpan.textContent = placeOneValue;
+    placeOneSpanTwo.textContent = placeOneValue;
+    yourNameSpan.textContent = yourNameValue;
+    nounItemOneSpan.textContent = nounItemOneValue;
+    nounItemOneSpanTwo.textContent = nounItemOneValue;
+    nounItemTwoSpan.textContent = nounItemTwoValue;
+    nounItemTwoSpanTwo.textContent = nounItemTwoValue;
+    adjectiveInputTwoSpan.textContent = adjectiveInputTwoValue;
+    adjectiveInputThreeSpan.textContent = adjectiveInputThreeValue;
+    animalOneSpan.textContent = animalOneValue;
+    verbOneSpan.textContent = verbOneValue;
+    adjectiveInputFourSpan.textContent = adjectiveInputFourValue;
+    adjectiveInputFiveSpan.textContent = adjectiveInputFiveValue;
+    nameTwoSpan.textContent = nameTwoValue;
+    nounOneSpan.textContent = nounOneValue;
+    nounTwoSpan.textContent = nounTwoValue;
+    nounPluralSpan.textContent = nounPluralValue;
+
+
 
     //console.log('something', nameOneValue); WORKS
 
